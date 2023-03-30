b3a602d171f9bc1dc40fe2c00a45222d1df8c480
JStroo2000 <127327594+JStroo2000@users.noreply.github.com>
2023-03-30 10:34:50 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,19 +5,13 @@ if [ ! -d "plot" ]; then
   echo "Directory does not exist create it!"
   mkdir plot
 fi
-
-
 # Script for excercise 1
 echo "Run the first script ..."
 python3 NUR_handin2_ex1.py > NUR_handin2_ex1.txt
-
 # Script for excercise 2
 echo "Run the second script ..."
 python3 NUR_handin2_ex2.py > NUR_handin2_ex2.txt
-
-
 echo "Generating the pdf"
-
 pdflatex main.tex
 bibtex main.aux
 pdflatex main.tex
